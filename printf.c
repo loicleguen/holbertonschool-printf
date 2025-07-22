36f37f3eefa3251cef645fcba15a892ede3aaef4
loicleguen <loic.leguen@hotmail.fr>
2025-07-22 11:45:56 +0200

    Implement custom _printf function for formatted output

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _printf - printf function
+ * @format: format string
+ * @...: number of parameters stored
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	if (!format)
+		return (-1);
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent();
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_int(args);
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
