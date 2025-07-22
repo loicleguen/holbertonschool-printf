2c6bd01449e59f1ac31d63ab9b1288179226571b
loicleguen <loic.leguen@hotmail.fr>
2025-07-22 11:42:09 +0200

    Header file

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
-int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
