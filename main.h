832d23cc225b43f83168f4e1cd06b3849f8ce342
loicleguen <loic.leguen@hotmail.fr>
2025-07-24 09:39:17 +0200

    declaration handle_format

--- main.h
+++ main.h
@@ -5,9 +5,11 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int handle_format(const char *format, int *i, va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
 int print_int(va_list args);
+
 
 #endif /* MAIN_H */
